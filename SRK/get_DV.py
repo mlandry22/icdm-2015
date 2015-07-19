a9179e6737b6646ef914afbfd4ea17deb0545d09
SudalaiRajkumar <ssraj.rox@gmail.com>
2015-07-19 09:54:17 +0530

    Update and rename getDV.py to get_DV.py

--- SRK/get_DV.py
+++ SRK/get_DV.py
@@ -1,3 +1,15 @@
+"""
+This code is to get the cookies corresponding to devices present in dev and val samples. The methodology is as follows:
+1. Read the 'cookie_all_basic.csv' file
+2. Prepare a 'drawbridge_cookie_dict' where drawbridge_handle is the key and list of cookies corresponding to the drawbridge_handle is the value
+3. Read the 'dev_dev_basic.csv' file. This file has info about the device ids and the drawbridge handles of dev sample.
+4. Open the 'dev_DV.csv' in write mode. 
+5. Loop through the 'dev_dev_basic' file. Each row in this file has a device_id and drawbridge_handle. Now using this drawbridge_handle, get the corresponding cookies using 'drawbridge_cookie_dict'.
+6. Write the device_id and cookies into the 'dev_DV.csv' file. Cookies are written in the form of list. Example:
+		device_id1, ['cookie_id1','cookie_id2']
+7. File 'dev_dev_basic.csv' is similar to the output file which we need to submit except for the fact that here cookies are put together in the form of list
+8. Repeat the same for validation sample to get the cookies associated with the devices present in val sample
+"""
 import csv
 import numpy as np
 import pandas as pd
@@ -21,10 +33,10 @@ if __name__ == "__main__":
 	cookie_reader.next() # skipping the header	
 	drawbridge_cookie_dict = {}
 	for row in cookie_reader:
-		if row[0] != "-1":
+		if row[0] != "-1": # -1 means drawbridge handles are missing 
 			temp_list = drawbridge_cookie_dict.get(row[0],[])
 			temp_list.append(row[1])
-			drawbridge_cookie_dict[row[0]] = temp_list[:]
+			drawbridge_cookie_dict[row[0]] = temp_list[:] # saving the cookies in a list format with drawbridge_handle as key
 	cookie_handle.close()
 
 	# getting the DV for the dev sample device IDs #
@@ -38,8 +50,8 @@ if __name__ == "__main__":
 	dev_DV_writer.writerow(["device_id", "cookie_id_list"])
 
 	for row in dev_device_reader:
-		device_id = row[1]
-		drawbridge_handle = row[0]
+		device_id = row[1] # second element is device id
+		drawbridge_handle = row[0] # first element is drawbridge handle
 		cookie_list = drawbridge_cookie_dict[drawbridge_handle] 
 		dev_DV_writer.writerow([device_id, cookie_list])
 	dev_device_handle.close()
